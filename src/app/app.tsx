44657c865efdd8db6ece9515093163b16f7f9e77
Hung Vu <hungvsme@gmail.com>
2024-01-05 23:56:15 -0600

    Add simple navbar with chakra ui

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
+import Landing from './pages/landing/landing';
+import Trophies from './pages/trophies/trophies';
+import Players from './pages/players/players';
+import Managers from './pages/managers/managers';
+import NavBar from './components/nav-bar/nav-bar';
 
 export function App() {
   return (
-    <div>
-      Ayo
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/trophies" element={<Trophies />}></Route>
+        <Route path="/players" element={<Players />}></Route>
+        <Route path="/managers" element={<Managers />}></Route>
+      </Routes>
+    </>
   );
 }
 
--- src/app/components/nav-bar/nav-bar.tsx
+++ src/app/components/nav-bar/nav-bar.tsx
@@ -1,13 +1,125 @@
-import styles from './nav-bar.module.css';
+// import styles from './nav-bar.module.css';
+import { Link } from 'react-router-dom';
+import { Flex, Button, IconButton } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
 
 /* eslint-disable-next-line */
 export interface NavBarProps {}
 
 export function NavBar(props: NavBarProps) {
+  const [display, changeDisplay] = useState('none');
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to NavBar!</h1>
-    </div>
+    <Flex>
+      <Flex margin={'auto'}>
+        <Flex display={['none', 'none', 'flex', 'flex']}>
+          <Link to={'/'}>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Home
+            </Button>
+          </Link>
+          <Link to={'/managers'}>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Managers"
+              my={5}
+              w="100%"
+            >
+              Managers
+            </Button>
+          </Link>
+          <Link to={'/players'}>
+            <Button as="a" variant="ghost" aria-label="Players" my={5} w="100%">
+              Players
+            </Button>
+          </Link>
+          <Link to={'/trophies'}>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Trophies"
+              my={5}
+              w="100%"
+            >
+              Trophies
+            </Button>
+          </Link>
+        </Flex>
+        <Flex w="100vw" justifyContent={'flex-end'} mt={2} display={['flex', 'flex', 'none', 'none']}>
+          <IconButton
+            aria-label="Open Menu"
+            size="lg"
+            mr={2}
+            icon={<HamburgerIcon />}
+            onClick={() => changeDisplay('flex')}
+          />
+        </Flex>
+      </Flex>
+
+      <Flex
+        w="100vw"
+        display={display}
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Open Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => changeDisplay('none')}
+          />
+        </Flex>
+
+        <Flex flexDir="column" align="center">
+          <Link to={'/'}>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Home
+            </Button>
+          </Link>
+
+          <Link to={'/managers'}>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Managers"
+              my={5}
+              w="100%"
+            >
+              Managers
+            </Button>
+          </Link>
+
+          <Link to={'/players'}>
+            <Button as="a" variant="ghost" aria-label="Players" my={5} w="100%">
+              Players
+            </Button>
+          </Link>
+
+          <Link to={'/trophies'}>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Trophies"
+              my={5}
+              w="100%"
+            >
+              Trophies
+            </Button>
+          </Link>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
