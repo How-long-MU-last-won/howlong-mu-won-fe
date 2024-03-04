952ec2f71115d5d961272b743fed0a6d34c40c07
Hung Vu <hungvsme@gmail.com>
2024-03-04 17:12:49 -0600

    Add collapse effect to Players Page

--- src/app/pages/players/player-container-by-position/player-container-by-position.tsx
+++ src/app/pages/players/player-container-by-position/player-container-by-position.tsx
@@ -1,6 +1,8 @@
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Box, Button, Collapse, SimpleGrid } from '@chakra-ui/react';
 import PlayerCard from '../player-card/player-card';
 import { PlayerObject } from 'src/types';
+import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
 
 /* eslint-disable-next-line */
 export interface PlayerContainerByPositionProps {
@@ -14,26 +16,45 @@ export function PlayerContainerByPosition(
   props: PlayerContainerByPositionProps
 ) {
   const { positionName, position, onOpen, players } = props;
+  const [isShowPlayer, setIsShowPlayer] = useState(true);
   return (
     <Box>
-      <Heading
+      <Button
         my={12}
-        as={'h3'}
         fontFamily={`'Montserrat', sans-serif;`}
         paddingBottom={'10px'}
+        fontSize={'2rem'}
+        background={'transparent'}
+        justifyContent={'flex-start'}
         w={'100%'}
         borderBottom={'1px solid #d9d9d9'}
+        onClick={() => setIsShowPlayer(!isShowPlayer)}
+        _hover={{
+          background: 'transparent',
+          color: 'bg.red',
+        }}
       >
+        {isShowPlayer ? (
+          <ChevronDownIcon boxSize={'2.5rem'} />
+        ) : (
+          <ChevronRightIcon boxSize={'2.5rem'} />
+        )}
         {positionName}
-      </Heading>
-      <SimpleGrid
-        minChildWidth={{ md: '30vw', lg: '24vw', xl: '20vw', xxl: '18vw' }}
-        spacing={{ base: '4vw', md: '2vw', lg: '1.5vw' }}
-      >
-        {players.filter((player) => player.position === position).map((player) => {
-          return <PlayerCard player={player} key={player.id} onOpen={onOpen} />;
-        })}
-      </SimpleGrid>
+      </Button>
+      <Collapse in={isShowPlayer} animateOpacity>
+        <SimpleGrid
+          minChildWidth={{ md: '30vw', lg: '24vw', xl: '20vw', xxl: '18vw' }}
+          spacing={{ base: '4vw', md: '2vw', lg: '1.5vw' }}
+        >
+          {players
+            .filter((player) => player.position === position)
+            .map((player) => {
+              return (
+                <PlayerCard player={player} key={player.id} onOpen={onOpen} />
+              );
+            })}
+        </SimpleGrid>
+      </Collapse>
     </Box>
   );
 }
